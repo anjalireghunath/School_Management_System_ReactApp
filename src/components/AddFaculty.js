c7bf2b05c3e86992fa28f28059050d72d22b880f
anjalireghunath <anjalireghu98@gmail.com>
2022-05-25 23:47:35 +0530

    faculty

--- src/components/AddFaculty.js
+++ src/components/AddFaculty.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import Header from './Header'
 
 const AddFaculty = () => {
-    var [name,setName]=useState("")
+    var [fname,setFname]=useState("")
     var [education,setEducation]=useState("")
-    var [mobile,SetMobile]=useState("")
-    var [address,setAddres]=useState("")
+    var [fmobile,SetFmobile]=useState("")
+    var [faddress,setFaddres]=useState("")
     var [pincode,setPincode]=useState("")
     var [district,setDistrict]=useState("")
     const facultyData=()=>{
-        const data={"name":name,"education":education,"mobile":mobile,"address":address,"pincode":pincode,"district":district}
+        const data={"fname":fname,"education":education,"mobile":fmobile,"address":faddress,"pincode":pincode,"district":district}
         console.log(data)
     }
   return (
@@ -21,7 +21,7 @@ const AddFaculty = () => {
             <div className="row g-3">
                 <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Name</label>
-                    <input onChange={(b)=>{setName(b.target.value)}} type="text" className="form-control"/>
+                    <input onChange={(b)=>{setFname(b.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Education</label>
@@ -34,12 +34,12 @@ const AddFaculty = () => {
                 </div>
                 <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Mobile</label> 
-                    <input onChange={(b)=>{SetMobile(b.target.value)}} type="text" className="form-control"/>  
+                    <input onChange={(b)=>{SetFmobile(b.target.value)}} type="text" className="form-control"/>  
                    
                 </div>
                 <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Address</label>
-                    <input onChange={(b)=>{setAddres(b.target.value)}} type="text" className="form-control"/>
+                    <input onChange={(b)=>{setFaddres(b.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Pincode</label>
