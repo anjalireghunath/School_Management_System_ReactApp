1bec6c26115412000a1670ea40bc37f07b9faf38
anjalireghunath <anjalireghu98@gmail.com>
2022-05-25 20:53:32 +0530

    add student

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AddStudent from './components/AddStudent';
 
 function App() {
   return (
    <div>
+
+<AddStudent/>
 
    </div>
   );
---
+++ src/components/AddStudent.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+const AddStudent = () => {
+    var [admno,setAdmno]=useState("")
+    var [rollno,setRollno]=useState("")
+    var [name,setName]=useState("")
+    var [classs,setclass]=useState("")
+    var [parentname,setParentName]=useState("")
+    var [mobile,setMobile]=useState("")
+    var [address,setAddress]=useState("")
+    const addStud=()=>{
+        const data={"admno":admno,"rollno":rollno,"name":name,"classs":classs,"parentname":parentname,"mobile":mobile,"address":address}
+        console.log(data)
+    }
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">AdmNo</label>
+                    <input onChange={(a)=>{setAdmno(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">RollNo</label>
+                    <input onChange={(a)=>{setRollno(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Name</label>
+                    <input onChange={(a)=>{setName(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">class</label>
+                    <input onChange={(a)=>{setclass(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Parent Name</label>
+                    <input onChange={(a)=>{setParentName(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Mobile</label>
+                    <input onChange={(a)=>{setMobile(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Address</label>
+                    <input onChange={(a)=>{setAddress(a.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={addStud} className="btn btn-primary">REGISTER</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default AddStudent
